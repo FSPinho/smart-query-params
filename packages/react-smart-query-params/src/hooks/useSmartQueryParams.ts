23691614ceccb54477e9bbe3061a7fed7f714e07
Felipe Pinho <felipe76857685@gmail.com>
2024-04-13 22:03:40 -0400

    Add lerna

--- packages/react-smart-query-params/src/hooks/useSmartQueryParams.ts
+++ packages/react-smart-query-params/src/hooks/useSmartQueryParams.ts
@@ -1,13 +1,19 @@
 import { useMemo } from 'react';
 import { Options, Schema } from 'smart-query-params';
 
-export function useSmartQueryParams<SCH extends Schema>(schema: Options<SCH>['schema']): void;
-export function useSmartQueryParams<SCH extends Schema>(options: Options<SCH>): void;
-export function useSmartQueryParams<SCH extends Schema>(schemaOrOptions: Options<SCH> | Options<SCH>['schema']): void {
+export interface UseSmartQueryParamsReturn {}
+
+export function useSmartQueryParams<SCH extends Schema>(schema: Options<SCH>['schema']): UseSmartQueryParamsReturn;
+export function useSmartQueryParams<SCH extends Schema>(options: Options<SCH>): UseSmartQueryParamsReturn;
+export function useSmartQueryParams<SCH extends Schema>(
+  schemaOrOptions: Options<SCH> | Options<SCH>['schema'],
+): UseSmartQueryParamsReturn {
   const options: Options<SCH> = useMemo(
     () => (isOptions(schemaOrOptions) ? schemaOrOptions : { schema: schemaOrOptions }),
     [],
   );
+
+  return {};
 }
 
 function isOptions<SCH extends Schema>(obj: any): obj is Options<SCH> {
